4e58ec1a783acfcbef43ec5d43aaa9b0ff9b3b6b
Vladimir Mikhalenko <mihalenko.w.a@gmail.com>
2020-07-02 19:05:21 +0300

    Add tab rearrangment sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
+interface Tab {
+  id: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'animation-bug';
+
+  public tabs: Tab[] = [
+    { id: 'tab1' },
+    { id: 'tab2' },
+    { id: 'tab3' }
+  ];
+
+  public trackById = (index: number, box: Tab) => box.id;
+
+  public onRearrange(): void {
+    this.tabs = this.tabs.slice();
+    const buf = this.tabs[0];
+    this.tabs[0] = this.tabs[1];
+    this.tabs[1] = buf;
+  }
+
 }
